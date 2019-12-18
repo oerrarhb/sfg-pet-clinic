764ec0897d773e4b931a830164da980c3706961b
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2019-12-18 21:53:48 +0100

    adding POJOs. Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private petType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public guru.springframework.sfgpetclinic.model.petType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(guru.springframework.sfgpetclinic.model.petType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+}
