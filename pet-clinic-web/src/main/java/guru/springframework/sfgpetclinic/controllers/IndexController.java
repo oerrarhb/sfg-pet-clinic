042f9fc3d6d2684541c1cc452082c4f7f05fcc19
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2020-02-10 14:55:51 +0100

    Added Layout Index Page. Closes #19.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping({"","/","index","index.html"})
     public String index() {
-        return "index";
+        return "welcome";
     }
 }
