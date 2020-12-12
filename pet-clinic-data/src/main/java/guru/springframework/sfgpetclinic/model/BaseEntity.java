12377738d12e8ef5d3edcb425fd8b57ad657a81f
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2020-12-12 17:44:33 +0100

    Updated base entity with JPA annotations. Closes #35

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,6 +1,10 @@
 package guru.springframework.sfgpetclinic.model;
 
 import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -14,6 +18,9 @@ import lombok.With;
 @Setter
 @Getter
 @With
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long Id;
 }
