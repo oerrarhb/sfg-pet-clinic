492721f83d842d508f781424e2bc7ef884d586e6
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2020-12-12 18:12:42 +0100

    Updated visit with JPA. Closes #38

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -1,21 +1,31 @@
 package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.With;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 @With
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 }
