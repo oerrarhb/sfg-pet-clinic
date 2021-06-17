48cbabeb7239cd763aec21ee499d7e1baa70f282
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2021-06-17 15:58:14 +0200

    Fix owner func. closes #62

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -44,7 +44,7 @@ public class OwnerController {
         if (Objects.equals(owner.getLastName(), null)) {
             owner.setLastName("");
         }
-        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
+        List<Owner> results = ownerService.findAllByLastNameLike("%" + owner.getLastName() + "%");
         if (results.isEmpty()) {
             result.rejectValue("lastName", "notFound", "not found");
             return "owners/findOwners";
